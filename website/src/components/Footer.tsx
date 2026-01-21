4be3c31a1fe7f4245ce145fd966c28a3eb2f9fc6
Aditya Thukral <adityathukral23@gmail.com>
2026-01-22 02:21:21 +0530

    all final

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -102,7 +102,7 @@ const Footer = () => {
       <div className="border-t border-white/10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
           <p className="text-center text-sm text-white/60">
-            © 2025 quickMarketing. All rights reserved. Licensed in UAE.
+            © 2026 QuickMarketing. All rights reserved. Licensed in UAE.
           </p>
         </div>
       </div>
